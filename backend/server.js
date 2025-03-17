daab552ec0d5271547323a27fc395c1453ef31a0
Bilal Djezzar <48605897+DjezzarBilal@users.noreply.github.com>
2025-03-17 12:32:41 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,11 @@ import userRoute from "./routes/user.route.js"
 import connectToMongoDB from "./db/connectToMongoDb.js";
 import { app, server } from "./socket/socket.js";
 
-const PORT = process.env.PORT || 5000;
 
 const __dirname= path.resolve();
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
